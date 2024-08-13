f83d2dcaf351a3a61933d722b373e04fdb3be28e
Jimmy Bowcott <151764691+JimmyBowcott@users.noreply.github.com>
2024-08-13 11:39:13 +0100

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+
+const Homepage = () => {
+    return(
+        <div className="flex flex-col h-screen w-screen items-center justify-center text-center">
+            <h1 className="text-7xl font-bold text-fuchsia-600">Wagwan</h1>
+        </div>
+    )
+}
+
+export default Homepage
